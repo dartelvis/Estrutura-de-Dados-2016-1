0c95c4b9003a848361684735ec5cd099fb7b67be
JeanFenner <jeancarlo.fenner@gmail.com>
2016-03-09 21:57:04 -0300

    Update AlocacaoDinamica1.c

--- AlocacaoDinamica1.c
+++ AlocacaoDinamica1.c
@@ -8,21 +8,19 @@ int main()
     rec = (int *)malloc(tam*sizeof(int));
 
     if(rec == NULL){
-        printf("Erro de alocação de memoria!");
+        printf("Erro de alocaÃ§Ã£o de memoria!");
     }else{
         for(i=0; i<=tam; i++){
             printf("Digite um valor: ");
             if(scanf("%d", &num)==0){
                 printf("FIM DO PROGRAMA!\n\n");
                 break;
-            } else{
-                if(i == tam){
-                    tam += 5;
-                    rec = (int *)realloc(rec, tam*sizeof(int));
-                }
-                rec[i] = num;
-                c++;
+            } else if(i == tam){
+                tam += 5;
+                rec = (int *)realloc(rec, tam*sizeof(int));
             }
+            rec[i] = num;
+            c++;
         }
         printf("Valores digitados\n");
         for(j=0; j<c; j++){
