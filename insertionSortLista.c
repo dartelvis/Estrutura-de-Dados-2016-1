b1e15b69d6884cc8f245e8d988be76073dac2851
JeanFenner <jeancarlo.fenner@gmail.com>
2016-05-12 13:59:13 -0300

    Add files via upload

---
+++ insertionSortLista.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int t;
+    time_t tI, tF;
+
+    tI = time(NULL);
+
+    inc = insertionSort(inc);
+
+    tF = time(NULL);
+    t = difftime(tF, tI);
+    printf("\n\n%d", t);
+
+    return 0;
+}
+
+int insertionSort(node *inc){
+    node *i, *j, *key;
+
+    for(j = inc->next; j!=NULL; j=j->next){
+        key = j;
+        i = j->prev;
+        while(i->prev!=NULL && i->val>key->val){
+            i->next->val = i->val;
+            i = i->prev;
+        }
+        i->next = key;
+    }
+    return num[0];
+}
